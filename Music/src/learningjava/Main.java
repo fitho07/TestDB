6bceb7a50c6b40cd59eaa3dd0da8d51437dd5329
fitho07 <lord_fuego.40@hotmail.com>
2022-11-09 22:54:10 -0700

    Setting connection with music.db

---
+++ Music/src/learningjava/Main.java
@@ -0,0 +1,14 @@
+package learningjava;
+
+import learningjava.model.Datasource;
+
+public class Main {
+    public static void main(String[] args) {
+        Datasource datasource = new Datasource();
+        if (!datasource.open()) {
+            System.out.println("Can't open datasource");
+            return;
+        }
+        datasource.close();
+    }
+}
